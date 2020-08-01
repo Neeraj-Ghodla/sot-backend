85e314a7a1852c9fd6abddcc4a5ac526d84d90f9
Neeraj Ghodla <neerajghodla111@gmail.com>
2020-08-02 10:31:25 +1200

    cafe and restaurant added, fake review, price, rating added, and return one random result

--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,64 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const parseString = require("xml2js").parseString;
+require("dotenv").config();
 
 const url = `http://www.zenbu.co.nz/findnearest.xml`;
 
 router.get("/", async (req, res) => {
   const lon = req.query.lon;
   const lat = req.query.lat;
-  const { data } = await axios.get(url, {
+
+  const { data: restaurantData } = await axios.get(url, {
     params: {
       x: lon,
       y: lat,
+      q: "restaurant",
       key: process.env.API_KEY,
     },
   });
-  parseString(data, (err, result) => {
-    if (err) res.json({ msg: err });
+
+  const { data: cafeData } = await axios.get(url, {
+    params: {
+      x: lon,
+      y: lat,
+      q: "cafe",
+      key: process.env.API_KEY,
+    },
+  });
+
+  data = [];
+
+  parseString(restaurantData, (err, result) => {
+    if (err) res.json({ status: "error converting xml to json", data: [] });
     try {
-      res.json(result["zenbu"]["entries"][0]["entry"]);
+      data = [...data, ...result["zenbu"]["entries"][0]["entry"]];
     } catch (err) {
-      res.json({ msg: "Not available" });
+      res.json({ status: "no data available", data: [] });
     }
+  });
+
+  parseString(cafeData, (err, result) => {
+    if (err) res.json({ status: "error converting xml to json", data: [] });
+    try {
+      data = [...data, ...result["zenbu"]["entries"][0]["entry"]];
+    } catch (err) {
+      res.json({ status: "no data available", data: [] });
+    }
+  });
+
+  // get the random index
+  const index = Math.floor(Math.random() * data.length);
+
+  // add the fake data
+  data[index]["review"] =
+    "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+  data[index]["price"] = Math.floor(1 + Math.random() * 3);
+  data[index]["rating"] = (1 + Math.random() * 4).toFixed(1);
+
+  res.json({
+    status: "okay",
+    data: data[index],
   });
 });
 
