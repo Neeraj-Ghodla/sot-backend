5df4398db49ed378119c9f9f01a82f30a45b7d28
Neeraj Ghodla <neerajghodla111@gmail.com>
2020-08-02 11:24:11 +1200

    cors middleware added

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const routes = require("./routes/routes");
 
 // initialize the app
 const app = express();
+
+// cors
+app.use(cors());
 
 // PORT
 const PORT = process.env.PORT || 3000;
