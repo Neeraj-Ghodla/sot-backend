81998e7c052da91189bcd835c82636c0b55e19c2
Neeraj Ghodla <neerajghodla111@gmail.com>
2020-08-01 14:30:08 +1200

    location endpoint

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const parseString = require("xml2js").parseString;
+
+const url = `http://www.zenbu.co.nz/findnearest.xml`;
+
+router.get("/", async (req, res) => {
+  const lon = req.query.lon;
+  const lat = req.query.lat;
+  const { data } = await axios.get(url, {
+    params: {
+      x: lon,
+      y: lat,
+      key: process.env.API_KEY,
+    },
+  });
+  parseString(data, (err, result) => {
+    if (err) res.json({ msg: err });
+    else res.json(result["zenbu"]["entries"][0]["entry"]);
+  });
+});
+
+module.exports = router;
